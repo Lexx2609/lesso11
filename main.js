4cf76a183b9c509ee6721a9dcb2f4ecde4b8a8e1
Lexx2609 <2433053@mail.ru>
2023-07-17 15:34:51 +0500

    promise new

--- main.js
+++ main.js
@@ -22,14 +22,31 @@ three.onclick = function () {
     text.innerHTML = `<h1>Вывод из кнопки номер 3:${e}</h1>`
     console.log('qwerty3')
 }
-let qwe = [1,2,3,4,5,6,7,8,9,10]
-let arr = new Set(qwe)
-console.log(arr)
-for (let i of arr){
-    if (i > 5){
-        let test = new Set([i])
-        console.log(test)
-    } else {
-        console.log(false)
+// let qwe = [1,2,3,4,5,6,7,8,9,10]
+// let arr = new Set(qwe)
+// console.log(arr)
+// for (let i of arr){
+//     if (i > 5){
+//         let test = new Set([i])
+//         test.add(i)
+//         console.log(test)
+//     } else {
+//         console.log(false)
+//     }
+// }
+// let a = []
+// if(arr >= 5){
+//     a.push(arr[key])
+// }
+let qwe = [4,6,20,20,35,183,0,2]
+let w = new Set(qwe)
+let show = document.querySelector('.div')
+show.onclick = function () {
+    let abc = []
+    for (let i of w) {
+        if (i > 5) {
+            abc.push(i)
+        }
     }
+    console.log(abc)
 }
